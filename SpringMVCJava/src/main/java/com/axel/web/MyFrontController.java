ac7f3e68e33ccf05e4fa3bf56aad198472debdb0
Axel Kouassi <ak659@njit.edu>
2020-05-01 19:14:22 -0400

    Spring MVC project using java configurations

---
+++ SpringMVCJava/src/main/java/com/axel/web/MyFrontController.java
@@ -0,0 +1,25 @@
+package com.axel.web;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyFrontController extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {MVC_Config.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+}
---
+++ SpringMVCJava/src/main/webapp/result.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1" isELIgnored="false"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Display of Result</title>
+</head>
+
+<h1>Result</h1>
+<body>
+
+    Result is: ${result}
+
+</body>
+</html>
